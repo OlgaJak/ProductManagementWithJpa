ec261bd5363f735537d87ed78f0e046aa8df8015
admin <boot309@gmail.com>
2023-07-15 13:37:46 +0300

    adding ProductRepository

--- src/main/java/com/productmanagementwithjpa/controller/ProductController.java
+++ src/main/java/com/productmanagementwithjpa/controller/ProductController.java
@@ -1,11 +1,19 @@
 package com.productmanagementwithjpa.controller;
 
+import com.productmanagementwithjpa.entity.ProductRequest;
+import com.productmanagementwithjpa.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
+    private ProductRepository productRepository;
+    @Autowired
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
     @GetMapping("/")
     public String displayProducts(){
         return "all-product";
@@ -16,7 +24,7 @@ public class ProductController {
     }
 
     @PostMapping("/create-product")
-    public String handleCreateProduct(){
+    public String handleCreateProduct(ProductRequest productRequest){
         //do stuff with the product here
         return "redirect:/";
     }
--- src/main/java/com/productmanagementwithjpa/repository/ProductRepository.java
+++ src/main/java/com/productmanagementwithjpa/repository/ProductRepository.java
@@ -4,4 +4,5 @@ import com.productmanagementwithjpa.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
+
 }

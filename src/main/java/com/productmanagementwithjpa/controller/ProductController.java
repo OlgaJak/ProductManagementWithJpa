59202640bb0613c68291baf2335a6e68d82b8ca9
admin <boot309@gmail.com>
2023-07-16 18:40:58 +0300

    editing and deleting is not working at the moment

--- src/main/java/com/productmanagementwithjpa/controller/ProductController.java
+++ src/main/java/com/productmanagementwithjpa/controller/ProductController.java
@@ -3,38 +3,41 @@ package com.productmanagementwithjpa.controller;
 import com.productmanagementwithjpa.entity.Product;
 import com.productmanagementwithjpa.entity.ProductRequest;
 import com.productmanagementwithjpa.repository.ProductRepository;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ProductController {
     private ProductRepository productRepository;
+
     @Autowired
-    public ProductController(ProductRepository productRepository){
+    public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
     @GetMapping("/")
-    public String displayProducts(Model model){
-        model.addAttribute("products",this.productRepository.findAll());
+    public String displayProducts(Model model) {
+        model.addAttribute("products", this.productRepository.findAll());
         System.out.println(this.productRepository.findAll());
         return "all-products";
     }
+
     @GetMapping("/create")
-    public String displayAddProductPage(){
+    public String displayAddProductPage() {
         return "add-product";
     }
 
     @PostMapping("/create-product")
-    public String handleCreateProduct(ProductRequest productRequest){
+    public String handleCreateProduct(ProductRequest productRequest) {
         Product productToCreate = new Product();
 
         //transfer information from request to entity
         productToCreate.setName(productRequest.getName());
-        productToCreate.setPrice(productToCreate.getPrice());
-        productToCreate.setQuantity(productToCreate.getQuantity());
+        productToCreate.setPrice(productRequest.getPrice());
+        productToCreate.setQuantity(productRequest.getQuantity());
         productToCreate.setImageUrl(productRequest.getImageUrl());
 
 
@@ -42,4 +45,33 @@ public class ProductController {
         //do stuff with the product here
         return "redirect:/";
     }
+
+    @GetMapping("/product/{id}")
+    public String productDetails(@PathVariable Long id, Model model) {
+        // Redirect to the product details page with the given product ID
+        Product current = this.productRepository.findProductById(id);
+        model.addAttribute("product", current);
+        return "product-details";
+    }
+
+
+
+    @DeleteMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        this.productRepository.deleteProductById(id);
+        return "redirect:/";
+    }
+
+
+
+
+//    @GetMapping("/delete/{id}")
+//    public String showDeleteConfirmation(@PathVariable Long id, Model model) {
+//        Product productToDelete = this.productRepository.findProductById(id);
+//        model.addAttribute("product", productToDelete);
+//        return "delete-product";
+//    }
+
+
+
 }
--- src/main/java/com/productmanagementwithjpa/repository/ProductRepository.java
+++ src/main/java/com/productmanagementwithjpa/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.productmanagementwithjpa.repository;
 
 import com.productmanagementwithjpa.entity.Product;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +13,12 @@ import java.util.List;
 public interface ProductRepository extends CrudRepository<Product, Long> {
 //public List<Product> getAllBy();
 
-//public List<Product> findProductByNameLike(String name);
+    public List<Product> findProductByNameLike(String name);
 
-//public List<Product> findProductByPriceBetween(double start, double end);
+    public List<Product> findProductByPriceBetween(double start, double end);
+    public  Product findProductById(Long id);
+
+    void deleteProductById(Long id);
+
+
 }
